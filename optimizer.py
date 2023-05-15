ac722e49765f40a7b00d1f81c2b49ea7244fd729
Ethan Miller <olxiwipectra@gmx.net>
2023-05-15 10:00:00 -0400

    Implement professional adaptive optimizer core

--- optimizer.py
+++ optimizer.py
@@ -1,46 +1,25 @@
 import torch
 from torch.optim import Optimizer
-
 class CustomAdaptiveOptimizer(Optimizer):
-    """
-    A professional implementation of a custom adaptive optimizer 
-    with weight-decay and momentum-based updates.
-    """
     def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0):
-        if not 0.0 <= lr: raise ValueError(f"Invalid learning rate: {lr}")
         defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
         super(CustomAdaptiveOptimizer, self).__init__(params, defaults)
-
     @torch.no_grad()
     def step(self, closure=None):
         loss = None
-        if closure is not None:
-            with torch.enable_grad():
-                loss = closure()
-
         for group in self.param_groups:
             for p in group['params']:
                 if p.grad is None: continue
-                grad = p.grad
-                if grad.is_sparse: raise RuntimeError('Optimizer does not support sparse gradients')
-                
                 state = self.state[p]
                 if len(state) == 0:
                     state['step'] = 0
-                    state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
-                    state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
-
+                    state['exp_avg'] = torch.zeros_like(p)
+                    state['exp_avg_sq'] = torch.zeros_like(p)
                 exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
                 beta1, beta2 = group['betas']
                 state['step'] += 1
-                
-                if group['weight_decay'] != 0:
-                    grad = grad.add(p, alpha=group['weight_decay'])
-
-                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
-                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
-                
-                denom = (exp_avg_sq.sqrt() / (1 - beta2 ** state['step']) ** 0.5).add_(group['eps'])
-                step_size = group['lr'] / (1 - beta1 ** state['step'])
-                p.addcdiv_(exp_avg, denom, value=-step_size)
+                exp_avg.mul_(beta1).add_(p.grad, alpha=1 - beta1)
+                exp_avg_sq.mul_(beta2).addcmul_(p.grad, p.grad, value=1 - beta2)
+                denom = exp_avg_sq.sqrt().add_(group['eps'])
+                p.addcdiv_(exp_avg, denom, value=-group['lr'])
         return loss
